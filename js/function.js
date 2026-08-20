cd998adc05dbaef4b826bcfae1037d256edc5f37
EngAsmaaRamadan <asmdrsnjd@gmail.com>
2026-08-20 19:13:08 +0300

    solution of problem

--- js/function.js
+++ js/function.js
@@ -1,14 +1,14 @@
-/*this folowing code doesn't work*/
+/*this folowing code doesn't work, because that i didnt give opacity 0 with display none in popup in*/
 function openPopup(){
 	popup.classList.add("active");
 	setTimeout(function(){
 		popup.classList.add("show");
-	},1000);
+	},1);
 }
 
 function closePopup(){
 	popup.classList.remove("show");
 		setTimeout(function(){
-			popup.classList.remove("show");
+			popup.classList.remove("active");
 		},1000);//1000 because of transition-duration of 1s
 }
--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ let popupKeys=document.querySelectorAll("#Gallery .popupkeys"),
 	popupBox=popup.querySelector(".box"),
 	indicatorContainer=popup.querySelector("indicators");
 	// indicatorsButton=indicatorContainer.createElement("li");
-/*this folowing code doesn't work*/
+
 popupKeys.forEach(function(popupKey){
 	popupKey.addEventListener("click",openPopup);
 });
 
 popup.addEventListener("click",closePopup);
 popupBox.addEventListener("click",function(e){
-	e.stopPropegation();//to not close the popup in click on the box
+	e.stopPropagation();//to not close the popup in click on the box
 })
 
 popupClose.addEventListener("click",closePopup);
